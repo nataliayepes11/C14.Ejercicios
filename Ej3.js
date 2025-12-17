74d26fee1011407d3119cf8e0d9034af51d8059b
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-17 13:47:59 -0300

    FUNCIÓN ARROW + IF/ELSE IF/ ELSE

--- Ej3.js
+++ Ej3.js
@@ -2,3 +2,22 @@
 // Solicita al usuario una edad y usa una función flecha para clasificarla en niño, adolescente, adulto o adulto mayor.
 
 const prompt = require('prompt-sync')();
+
+console.log( "--- CLASIFICACIÓN DE EDADES ---")
+let edad = Number(prompt("Ingresá tu edad: "));
+
+let clasificarEdad = (edad) => {
+    if (edad < 13) {
+        return "NIÑA/O";
+    } else if (edad < 18) {
+        return "ADOLESCENTE";
+    } else {
+        return "ADULTA/O";
+    }
+};
+
+console.log( "Sos " + clasificarEdad(edad));
+
+
+
+
