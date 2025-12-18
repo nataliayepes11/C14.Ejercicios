206a1c0bcea433e5e2467b77b5b6a449fe555507
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-18 10:52:54 -0300

    OBJETOS LITERALES + THIS

--- Ej6.js
+++ Ej6.js
@@ -1,6 +1,8 @@
 /* Ejercicio 6: El deportista
-Para este ejercicio deberán definir un objeto literal “deportista”, con los siguientes atributos: nombre, energía, experiencia.
-Ademas queremos poder pedirle al deportista que entrene. Para esto, nuestro trabajo va a ser realizar una función “entrenarHoras.”
+Para este ejercicio deberán definir un objeto literal “deportista”, 
+con los siguientes atributos: nombre, energía, experiencia.
+Ademas queremos poder pedirle al deportista que entrene. 
+Para esto, nuestro trabajo va a ser realizar una función “entrenarHoras.”
 La función entrenarHoras tiene las siguientes tres características:
 ✔	Recibe por parámetro la cantidad de horas.
 ✔	Resta a su energía (this.energia) la cantidad de horas x 5.
@@ -8,4 +10,23 @@ La función entrenarHoras tiene las siguientes tres características:
 Una vez definida la función, podemos ejecutar el código y ver cómo va variando la energía y experiencia del deportista por consola.
 */
 
-const prompt = require('prompt-sync')();
+const deportista = {
+    nombre: "Juan",
+    energia: 92,
+    experiencia: 8,
+
+    entrenarHoras: function (horas) {
+        this.energia = this.energia - (horas * 5);
+        this.experiencia = this.experiencia + (horas * 2);
+    }
+};
+
+deportista.entrenarHoras(2);
+
+console.log("--- ESTADO DEL DEPORTISTA ---");
+console.log("Nombre:", deportista.nombre);
+console.log("Energía:", deportista.energia);
+console.log("Experiencia:", deportista.experiencia);
+
+
+
